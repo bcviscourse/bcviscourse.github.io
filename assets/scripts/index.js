30381cc684c6ca76141ab922c6422d054b09811c
Nam Wook Kim <namwkim85@gmail.com>
2019-08-14 18:25:52 -0400

    adding descriptions

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,7 @@
 let dayMap = {'Sun':0, 'Mon':1, 'Tue':2, 'Wed':3, 'Thu':4, 'Fri':5, 'Sat': 6};
 let revDayMap = Object.entries(dayMap).reduce((acc, [k,v])=>Object.assign({}, acc, {[v]:k}), {})
 document.querySelector('#copyright-year').innerHTML = (new Date()).getFullYear()
-fetch('assets/schedule.json').then(resp=>resp.json()).then(course=>{
+fetch('assets/syllabus.json').then(resp=>resp.json()).then(course=>{
     console.log('course', course);
     let container = document.querySelector('.schedule');
 
@@ -15,22 +15,22 @@ fetch('assets/schedule.json').then(resp=>resp.json()).then(course=>{
         let move = i%2? (7*(i-1)/2+delta):7*i/2;
         let curDay = new Date(startDay.valueOf());
         curDay.setDate(curDay.getDate() + move);
-        let child = renderClass(classDay, curDay);
+        let child = renderDaySchedule(classDay, curDay);
         container.appendChild(child);
 
     });
 });
 
-function renderClass(classDay, curDay){
+function renderDaySchedule(daySchedule, curDay){
     let div = document.createElement('div');
     div.classList.add('class');
     div.innerHTML =  `
-        <div class="classHeader">
-            <span class="date">${revDayMap[curDay.getDay()]} ${curDay.getMonth()}/${curDay.getDate()}</span>
-            <strong>${classDay.title}</strong>
+        <div class="header">
+            <span class="date">${revDayMap[curDay.getDay()]}, ${curDay.getMonth()}/${curDay.getDate()}</span>
+            <strong>${daySchedule.title}</strong>
         </div>
-        <div class="classContent">
-        TBD
+        <div class="content">
+            <p class="desc">${daySchedule.desc}</p>
         </div>
         `;
     return div;
