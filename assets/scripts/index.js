82d3125e8275a4f7b909d11c815e21a9b4d12186
Nam Wook Kim <namwkim85@gmail.com>
2019-08-15 08:15:50 -0400

    update descriptions

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,28 +9,29 @@ fetch('assets/syllabus.json').then(resp=>resp.json()).then(course=>{
 
     let delta = dayMap[days[1]] - dayMap[days[0]];
     let startDay = new Date(Date.parse(course.info.start_date));
-    console.log('start date', revDayMap);
+    console.log('start date', startDay);
     let toggle = true;
     course.schedule.forEach((classDay,i)=>{
         let move = i%2? (7*(i-1)/2+delta):7*i/2;
         let curDay = new Date(startDay.valueOf());
-        curDay.setDate(curDay.getDate() + move);
+        console.log('start date', curDay, move);
+        curDay.setDate(startDay.getDate() + move);
         let child = renderDaySchedule(classDay, curDay);
         container.appendChild(child);
 
     });
 });
 
-function renderDaySchedule(daySchedule, curDay){
+function renderDaySchedule(d, curDay){
     let div = document.createElement('div');
     div.classList.add('class');
     div.innerHTML =  `
         <div class="header">
-            <span class="date">${revDayMap[curDay.getDay()]}, ${curDay.getMonth()}/${curDay.getDate()}</span>
-            <strong>${daySchedule.title}</strong>
+            <span class="date">${revDayMap[curDay.getDay()]}, ${curDay.getMonth()+1}/${curDay.getDate()}</span>
+            <strong>${d.is_lab? '⚒':'📖'} ${d.title}</strong>
         </div>
         <div class="content">
-            <p class="desc">${daySchedule.desc}</p>
+            <p class="desc">${d.desc}</p>
         </div>
         `;
     return div;
