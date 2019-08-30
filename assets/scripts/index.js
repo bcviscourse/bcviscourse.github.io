84f6c3599a78c24660723b53bfd041ee411f2db0
Nam Wook Kim <namwkim85@gmail.com>
2019-08-30 14:11:51 -0400

    add preset filters

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,27 +4,36 @@ document.querySelector('#copyright-year').innerHTML = (new Date()).getFullYear()
 
 let syllabus;
 fetch('assets/syllabus.json').then(resp=>resp.json()).then(course=>{
-    console.log('course', course);
     syllabus = course;
     syllabus.weeks = groupByWeek(syllabus);
     renderSchedule(syllabus.weeks);
     
 
 });
+let formatDate = (d)=>`${revDayMap[d.getDay()]}, ${d.getMonth()+1}/${d.getDate()}`
 function groupByWeek(syllabus){
     let days = syllabus.info.days.split('/');
 
     let delta = dayMap[days[1]] - dayMap[days[0]];
     let startDay = new Date(Date.parse(syllabus.info.start_date));
-    let toggle = true;
+    let lecture = 1;
+    let lab = 1;
     let groupped = syllabus.schedule.map((classDay,i)=>{
         
         let move = i%2? (7*(i-1)/2+delta):7*i/2;
         let curDay = new Date(startDay.valueOf());
-        // console.log('start date', curDay, move);
         curDay.setDate(startDay.getDate() + move);
         classDay.week = `Week ${parseInt(i/2+1)}`;
-        classDay.date = `${revDayMap[curDay.getDay()]}, ${curDay.getMonth()+1}/${curDay.getDate()}`;
+        classDay.date = curDay;
+        
+        if (classDay.type==="lab"){
+            classDay.index = lab;
+            lab+=1;
+        }else if (classDay.type==="lecture"){
+            classDay.index = lecture
+            lecture+=1;
+        }    
+
         return classDay;
     }).reduce((acc,d,i)=>{
         if (acc[d.week]){
@@ -54,7 +63,6 @@ function renderSchedule(weeks){
         });
         // let move = i%2? (7*(i-1)/2+delta):7*i/2;
         // let curDay = new Date(startDay.valueOf());
-        // // console.log('start date', curDay, move);
         // curDay.setDate(startDay.getDate() + move);
         // let child = renderDaySchedule(classDay, curDay);
         // container.appendChild(child);
@@ -68,9 +76,9 @@ function renderDaySchedule(d){
     div.classList.add('class');
     div.innerHTML =  `
         <div class="header" id="${d.title}">
-            <span class="date">${d.date}</span>
-            <strong>${d.is_lab? '⚒':'📖'} ${d.title}</strong>
-            &nbsp;<span>${d.slides?`(<a href="${d.slides}" target="_blank">Slides</a>)`:''}</span>
+            <span class="date">${formatDate(d.date)}</span>
+            <strong>${d.type==="lab"? '⚒ Lab':(d.type==="lecture"?'📖 Lecture':'')} ${d.index?d.index+'.':''} ${d.title}</strong>
+            &nbsp;<span>${d.slides?`<a href="${d.slides}" target="_blank">Slides</a>`:''}</span>
         </div>
         <div class="content">
             <p class="desc">${d.desc}</p>
@@ -86,7 +94,7 @@ function renderReadings(d){
     return `<ul> ${d.reduce((output, reading, i)=>{
         output+=`<li>
             ${(reading.required?'<strong>Required</strong>':'Optional')}:
-            (<a class='reading-title' target='_blank' href='${reading.link}'>${reading.title}</a>)
+            <a class='reading-title' target='_blank' href='${reading.link}'>${reading.title}</a>
             
         </li>`
         return output;
@@ -97,7 +105,6 @@ let newsSearch = document.querySelector('.search input[name="schedule"');
 
 newsSearch.addEventListener('input', function(event){
     // renderNews(allNews.filter(''))
-    // console.log('value', this.value);
     if (this.value!=''){
         let filtered = [];
         syllabus.weeks.forEach((week,i)=>{
@@ -106,7 +113,8 @@ newsSearch.addEventListener('input', function(event){
             }else{// filter individual schedule
                 let copy = {...week};
                 copy.schedule = copy.schedule.filter(d=>{
-                    return (d.date + ' ' + d.title).toLowerCase().includes(this.value.toLowerCase());
+                    let type = `${d.type==="lab"? 'Lab':(d.type==="lecture"?'Lecture':'')} ${d.index?d.index+'. ':''}`
+                    return (formatDate(d.date) + ' ' + type + d.title).toLowerCase().includes(this.value.toLowerCase());
                 })
                 filtered.push(copy);
             }
@@ -116,3 +124,46 @@ newsSearch.addEventListener('input', function(event){
         renderSchedule(syllabus.weeks);
     }
 });
+
+let conds = document.querySelectorAll('.filter .chip');
+
+conds.forEach(cond=>cond.addEventListener('click', function(event){
+    if (this.classList.contains('selected')==false){
+        let selected = document.querySelector('.chip.selected');
+        selected.classList.remove('selected');
+        this.classList.add('selected');   
+        let dataCond = this.dataset.cond;
+        let filtered = [];
+        switch (dataCond){
+            case 'all-schedule':
+                filtered= syllabus.weeks;
+                break;
+            case 'next-schedule':
+                let now = new Date();
+                syllabus.weeks.find((week,i)=>{
+                    let copy = {...week};
+                    copy.schedule = copy.schedule.filter(d=>d.date>now);
+                    filtered.push(copy);
+                    return copy.schedule.length>0? true:false;
+                })
+                break;
+            case 'lecture-schedule':
+                syllabus.weeks.forEach((week,i)=>{
+                    let copy = {...week};
+                    copy.schedule = copy.schedule.filter(d=>d.type==='lecture');
+                    filtered.push(copy);
+                })
+                break;
+            case 'lab-schedule':
+                syllabus.weeks.forEach((week,i)=>{
+                    let copy = {...week};
+                    copy.schedule = copy.schedule.filter(d=>d.type==='lab');
+                    filtered.push(copy);
+                })
+                break;
+            
+        }
+        renderSchedule(filtered);
+
+    }
+}));
